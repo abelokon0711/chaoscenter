394203b7201aaa4ba5cf6971e45923ddb78f8739
albeit06@hs-esslingen.de <abelokon@bernport.de>
2019-03-31 21:29:37 +0200

    initial commit, project setup with Spring Initializr

---
+++ src/main/java/com/studienarbeit/chaoscenter/ChaoscenterApplication.java
@@ -0,0 +1,13 @@
+package com.studienarbeit.chaoscenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaoscenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaoscenterApplication.class, args);
+	}
+
+}
