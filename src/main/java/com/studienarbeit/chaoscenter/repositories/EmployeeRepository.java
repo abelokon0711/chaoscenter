9ee8aa6beee9d9c95e0cb551e86b747cfd88410a
Alexander Belokon <alexander.belokon.mail@gmail.com>
2019-04-26 20:15:34 +0200

    added Javascript modules

--- src/main/java/com/studienarbeit/chaoscenter/repositories/EmployeeRepository.java
+++ src/main/java/com/studienarbeit/chaoscenter/repositories/EmployeeRepository.java
@@ -1,10 +1,10 @@
 package com.studienarbeit.chaoscenter.repositories;
 
 import com.studienarbeit.chaoscenter.entities.Employees;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "employees")
-public interface EmployeeRepository extends CrudRepository<Employees, Integer> {
+public interface EmployeeRepository extends PagingAndSortingRepository<Employees, Integer> {
 
 }
