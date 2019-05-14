2d067cf197df690269ac03188ef17bc137bec63e
Alexander Belokon <alexander.belokon.mail@gmail.com>
2019-05-15 00:09:14 +0200

    registered employees-service

--- src/main/java/com/studienarbeit/chaoscenter/services/employees/EmployeesServer.java
+++ src/main/java/com/studienarbeit/chaoscenter/services/employees/EmployeesServer.java
@@ -2,8 +2,10 @@ package com.studienarbeit.chaoscenter.services.employees;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmployeesServer {
 
 	public static void main(String[] args) {
