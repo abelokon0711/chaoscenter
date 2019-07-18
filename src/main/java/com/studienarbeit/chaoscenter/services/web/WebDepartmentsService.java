7827deff117c51b21a5a9efb46fffba739d5aba7
Alexander Belokon <alexander.belokon.mail@gmail.com>
2019-07-18 09:38:33 +0200

    included and enabled chaos monkey into the project

--- src/main/java/com/studienarbeit/chaoscenter/services/web/WebDepartmentsService.java
+++ src/main/java/com/studienarbeit/chaoscenter/services/web/WebDepartmentsService.java
@@ -4,10 +4,11 @@ import com.studienarbeit.chaoscenter.services.shared.entities.Departments;
 import com.studienarbeit.chaoscenter.services.web.resources.DepartmentsResources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import java.util.Collection;
 
-
+@Service
 public class WebDepartmentsService {
     @Autowired
     protected RestTemplate restTemplate;
