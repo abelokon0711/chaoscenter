0dffeb42b943e942030b20ab26c7c9c494eed5f4
Alexander Belokon <alexander.belokon@technicsatelier.com>
2019-07-22 11:34:56 +0200

    now fixed the babel issue for real

--- src/main/js/components/EmployeeList.js
+++ src/main/js/components/EmployeeList.js
@@ -12,6 +12,9 @@ export default class EmployeeList extends React.Component {
         <tbody>
           <tr>
             <th scope={"rowgroup"}>Employees - Loading time: {this.props.employees.responsetime ? this.props.employees.responsetime + " ms" : "-"}</th>
+            <th>
+              <button type="button" className="btn btn-light">Light</button>
+            </th>
           </tr>
           <tr>
             <th scope={"col"}>First Name</th>
